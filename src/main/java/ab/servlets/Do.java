d06ed4064373b247cb4a51cde27d64c60ae9db14
Pierre <pierre.bazonnard@gmail.com>
2016-03-20 17:22:38 +0100

    + servlets subfolder

--- src/main/java/ab/servlets/Do.java
+++ src/main/java/ab/servlets/Do.java
@@ -1,4 +1,4 @@
-package ab;
+package ab.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public final class Do extends HttpServlet {
 	
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
 	@Override
     public void doGet(HttpServletRequest request,
@@ -25,9 +25,6 @@ public final class Do extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         Map<String, String> m = new HashMap<String, String>();
         try (PrintWriter writer = response.getWriter()) {
-
-        	String[] parts = request.getQueryString().split("&");
-        	
             writer.println("<!DOCTYPE html><html>");
             writer.println("<head>");
             writer.println("<meta charset=\"UTF-8\" />");
@@ -38,12 +35,14 @@ public final class Do extends HttpServlet {
             writer.println("<h1>Request</h1>");
             writer.println("<p>URI   : " + request.getRequestURI() + "</p>");
             writer.println("<p>URL   : " + request.getRequestURL() + "</p>");
-            writer.println("<p>Query : " + request.getQueryString() + "</p>");
-
-            for(Integer i=0; i<parts.length; i++) {
-            	String[] sub = parts[i].split("=");
-            	m.put(sub[0], sub[1]);
-            	writer.println("<p>*&nbsp;" + sub[0] + "-->" + sub[1] + "</p>");
+            if(request.getQueryString() != null) {
+            	String[] parts = request.getQueryString().split("&");
+            	
+            	for(Integer i=0; i<parts.length; i++) {
+            		String[] sub = parts[i].split("=");
+            		m.put(sub[0], sub[1]);
+            		writer.println("<p>*&nbsp;" + sub[0] + "-->" + sub[1] + "</p>");
+            	}
             }
             writer.println("</body>");
             writer.println("</html>");
